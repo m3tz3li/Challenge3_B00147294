61e1de73f523fc952b1e8988f8b5d6cc98d0cca9
m3tz3li <b00147294@mytudublin.ie>
2022-10-21 12:54:04 +0100

    Challenge3

---
+++ Chalenge3B00147294/Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,65 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void FollowPlayer::Start()
+extern void FollowPlayer_Start_m138697E0740DB2570074B2DB69AE32AF46A994F9 (void);
+// 0x00000002 System.Void FollowPlayer::LateUpdate()
+extern void FollowPlayer_LateUpdate_m69B79F77CFC8072526CE0BA2D71179F66AE15B70 (void);
+// 0x00000003 System.Void FollowPlayer::.ctor()
+extern void FollowPlayer__ctor_mFEF642F6D296A2FBA30A01B637B6BD8DD2446114 (void);
+// 0x00000004 System.Void PlayerC::Start()
+extern void PlayerC_Start_m306D150472CDD7BDE81636AD4B9C082C6800AC14 (void);
+// 0x00000005 System.Void PlayerC::Update()
+extern void PlayerC_Update_mEC695DDA3CB43EBC30F038221359071FAFACFA36 (void);
+// 0x00000006 System.Void PlayerC::.ctor()
+extern void PlayerC__ctor_mC29CD5E5F193A269A0CCA56A25BB10BE77298985 (void);
+static Il2CppMethodPointer s_methodPointers[6] = 
+{
+	FollowPlayer_Start_m138697E0740DB2570074B2DB69AE32AF46A994F9,
+	FollowPlayer_LateUpdate_m69B79F77CFC8072526CE0BA2D71179F66AE15B70,
+	FollowPlayer__ctor_mFEF642F6D296A2FBA30A01B637B6BD8DD2446114,
+	PlayerC_Start_m306D150472CDD7BDE81636AD4B9C082C6800AC14,
+	PlayerC_Update_mEC695DDA3CB43EBC30F038221359071FAFACFA36,
+	PlayerC__ctor_mC29CD5E5F193A269A0CCA56A25BB10BE77298985,
+};
+static const int32_t s_InvokerIndices[6] = 
+{
+	984,
+	984,
+	984,
+	984,
+	984,
+	984,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	6,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
